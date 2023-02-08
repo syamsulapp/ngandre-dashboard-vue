e8d169fa9dc243b41b01aa778ee73ac8d2b48320
syamsulapp <samsul.marif100@gmail.com>
2023-02-08 11:01:44 +0800

    init vue dashboard ngandre

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h2>halo</h2>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/SamsulView.vue
@@ -0,0 +1,29 @@
+<template>
+    <h2>products data</h2>
+    <br>
+    <div>
+        <ul v-for="products in product" :key="products.id">
+            <li>{{ products.nama }} <br>
+                {{ products.imagesUrl }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            product: []
+        }
+    },
+
+    async mounted() {
+        const response = await axios.get('http://stagging.mjcutter.com/products')
+        this.product = response.data.data
+    }
+}
+
+</script>
+
